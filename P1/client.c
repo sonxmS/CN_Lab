2b2f9d85ccab731dc2475cad1402f85a6dc38c70
sonxmS <sonam.mec@gmail.com>
2025-10-04 16:07:26 +0530

    completed P2

--- P1/client.c
+++ P1/client.c
@@ -5,22 +5,31 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-void main(){
-    int buffer;
+int main() {
+    int client;
     char buffer[1024];
     struct sockaddr_in servAddr;
-    struct sockaddr_storage servAddr;
-    socklen_t addrSize;
-    client = socket(AF_INET,SOCK_STREAM,0);
+    client = socket(AF_INET, SOCK_STREAM, 0);
+    if (client < 0) {
+        perror("Socket creation failed");
+        return 1;
+    }
+
     servAddr.sin_family = AF_INET;
     servAddr.sin_port = htons(6265);
-    servAddr.sin_addr.s_addr = inet_addr("127.0.1");
+    servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    connect(client,(struct sockaddr *)&servAddr,sizeof(servAddr));
-    printf("1.Sending data to server\n")
-    strcpy(buffer,"Hi This is client\n");
-    send(client,buffer,19,0);
-    recv(client,buffer,1024,0);
-    printf("4.Data recieved:%s",buffer);
+    if (connect(client, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
+        perror("Connection failed");
+        return 1;
+    }
+    printf("Enter data to send: ");
+    fgets(buffer,sizeof(buffer),stdin);
+    printf("Sending data to server\n");
+    // strcpy(buffer, "Hi This is client");
+    send(client, buffer, strlen(buffer), 0);
+    recv(client, buffer, sizeof(buffer), 0);
+    printf("Data received: %s\n", buffer);
     close(client);
+    return 0;
 }
--- P1/server.c
+++ P1/server.c
@@ -5,30 +5,41 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-void main(){
-    int server,newSock;
+int main() {
+    int server, newSock;
     char buffer[1024];
-    struct sockaddr_storage servAddr;
-    struct sockaddr_storage store;
+    struct sockaddr_in servAddr; 
+    struct sockaddr_in store;
     socklen_t addrSize;
-    server = socket(AF_INET,SOCK_STREAM,0);
+    server = socket(AF_INET, SOCK_STREAM, 0);
+    if (server < 0) {
+        perror("Socket creation failed");
+        return 1;
+    }
     servAddr.sin_family = AF_INET;
-    servAddr.sin_port = htons(6265);
-    servAddr.sin_addr.s_addr = inet_addr("127.0.1");
-
-    bind(server,(struct sockaddr *)&servAddr,sizeof(servAddr));
-    if(listen(server,5)==0){
-        printf("Listening\n");
+    servAddr.sin_port = htons(6265); 
+    servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    if (bind(server, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
+        perror("Bind failed");
+        return 1;
     }
-    else{
-        printf("Error\n");
+    if (listen(server, 5) == 0) {
+        printf("Listening...\n");
+    } else {
+        printf("Error in listening\n");
     }
-    newSock = accept(server,(struct sockaddr*)&store,&addrSize);
-    recv(newSock,buffer,1024,0);
-    printf("2.Data recieved:%s",buffer);
-    strcpy(buffer,"Hi This is server\n");
-    printf("3.Sending data to client...\n");
-    send(newSock,buffer,19,0);
+    addrSize = sizeof(store);
+    newSock = accept(server, (struct sockaddr *)&store, &addrSize);
+    if (newSock < 0) {
+        perror("Accept failed");
+        return 1;
+    }
+    recv(newSock, buffer, sizeof(buffer), 0);
+    printf("Data received: %s\n", buffer);
+    strcpy(buffer, "Hi This is server\n");
+    printf("Sending data to client...\n");
+    send(newSock, buffer, strlen(buffer), 0);
     close(newSock);
     close(server);
+    return 0;
 }
